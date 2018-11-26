464044f922a1eaa5dc43de8a18a6af49452a07ae
BT_1N3_17@BKTECH.LOCAL <BT_1N3_17@BKTECH.LOCAL>
2018-11-26 14:46:34 -0500

    why do i even bother with this class at this point

---
+++ src/JackOLantern.java
@@ -0,0 +1,18 @@
+public class JackOLantern {
+
+    public JackOLantern(String[][] faceFeatures){
+        this.faceFeatures = faceFeatures;
+    }
+
+    public void edit(String replace, int row, int column){
+
+    }
+
+    public void fill(String str){
+
+    }
+
+    public String toString(){
+
+    }
+}
---
+++ src/Runner.java
@@ -0,0 +1,24 @@
+public class Runner {
+    public static void main(String[] args){
+        String[][] face = new String[5][8];
+        JackOLantern face1 = new JackOLantern(face);
+
+        face1.fill();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        System.out.println(face1);
+    }
+}
