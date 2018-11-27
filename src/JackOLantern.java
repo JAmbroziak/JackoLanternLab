c679ac694146f7367d674619835b3cc11de03079
BT_1N3_17 <jambroziak9340@bths.edu>
2018-11-27 14:47:17 -0500

    ok

--- src/JackOLantern.java
+++ src/JackOLantern.java
@@ -1,18 +1,30 @@
 public class JackOLantern {
+    private String[][] faceFeatures;
 
     public JackOLantern(String[][] faceFeatures){
         this.faceFeatures = faceFeatures;
     }
 
-    public void edit(String replace, int row, int column){
-
+    public void fill(String str){
+        for(int i = 0; i < faceFeatures.length; i++){
+            for(int j = 0; j < faceFeatures[i].length; j++){
+                faceFeatures[i][j] = str;
+            }
+        }
     }
 
-    public void fill(String str){
-
+    public void edit(String replace, int row, int column){
+        faceFeatures[row][column] = replace;
     }
 
     public String toString(){
-
+        String end = "";
+        for(int i = 0; i < faceFeatures.length; i++){
+            for(int j = 0; j <faceFeatures[i].length; j++){
+                end += faceFeatures[i][j];
+            }
+            end += "\n";
+        }
+        return end;
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,24 +1,48 @@
 public class Runner {
     public static void main(String[] args){
-        String[][] face = new String[5][8];
+        String[][] face = new String[10][15];
         JackOLantern face1 = new JackOLantern(face);
 
-        face1.fill();
-        face1.edit();
-        face1.edit();
-        face1.edit();
-        face1.edit();
+        face1.fill("0");
+        face1.edit(" ", 0, 0);
+        face1.edit(" ", 0, 1);
+        face1.edit(" ", 0, 2);
+        face1.edit(" ", 0, 3);
+        face1.edit(" ", 0, 4);
+        face1.edit(" ", 0, 5);
+        face1.edit(" ", 0, 6);
+        face1.edit(" ", 0, 7);
+        face1.edit(" ", 0, 13);
+        face1.edit(" ", 0, 14);
+        face1.edit(" ", 1, 0);
+        face1.edit(" ", 1, 1);
+        face1.edit(" ", 1, 2);
+        face1.edit(" ", 1, 3);
+        face1.edit(" ", 1, 4);
+        face1.edit(" ", 1, 5);
+        face1.edit(" ", 1, 6);
+        face1.edit(" ", 1, 14);
+        face1.edit(" ", 2, 0);
+        face1.edit(" ", 2, 1);
+        face1.edit(" ", 2, 2);
+        face1.edit(" ", 2, 3);
+        face1.edit(" ", 2, 4);
+        face1.edit(" ", 2, 5);
+        face1.edit(" ", 2, 6);
+        face1.edit(" ", 0, 3);
+        face1.edit(" ", 0, 3);
+        face1.edit(" ", 0, 3);
+        face1.edit(" ", 0, 3);
+        face1.edit(" ", 0, 3);
+        face1.edit(" ", 0, 3);
+        face1.edit(" ", 0, 3);
+        face1.edit(" ", 0, 3);
+        face1.edit(" ", 0, 3);
+        face1.edit(" ", 0, 3);
+        face1.edit(" ", 0, 3);
 
-        face1.edit();
-        face1.edit();
-        face1.edit();
-        face1.edit();
-        face1.edit();
-        face1.edit();
-        face1.edit();
-        face1.edit();
-        face1.edit();
-        face1.edit();
+
+
         System.out.println(face1);
     }
 }
